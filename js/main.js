472f6fe7adf8b3d431ded79b8559ccd97ef7f01b
Vitaly Polyakov <vpolyakov.mog@gmail.com>
2020-02-11 23:36:33 +0300

    desktop version of the menu

--- js/main.js
+++ js/main.js
@@ -1,37 +1,20 @@
+
 window.addEventListener('DOMContentLoaded', function() {
 
 	'use strict';
 
 	let 
+		burger = document.querySelector('.main-screen__burger'),
 		menu = document.querySelector('.main-screen__menu'),
-		logo = document.querySelector('.main-screen__logo');
-
-	logo.addEventListener('click', function() {
-
-		menu.classList.toggle('active');
-
-	});	
+		drawingBurger = document.querySelector('.main-screen__drawing');
 
 
-	let 
-		menuList = document.querySelectorAll('.main-screen__list'),
-		form = document.querySelector('.main-screen__form'),
-		closeForm = document.querySelector('.close-form');
+		burger.addEventListener('click', () => {
 
-	menuList[2].addEventListener('click', function(event) {
-		
-		form.classList.toggle('active');
-		menu.classList.remove('active');
-	});	
-	closeForm.addEventListener('click', function() {
-		form.classList.remove('active');
-	});
+			menu.classList.toggle('active');
+			drawingBurger.classList.toggle('active');
+			burger.classList.toggle('active');
 
-
-	[].forEach.call(menuList, function(item) {
-		item.addEventListener('click', function() {
-			console.log('Вы кликнули' + item);
-		});
 	});
 
 });
